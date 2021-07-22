3277f7399341328a931af8c538edf4300fdecffc
kemokemo <t2wonderland@gmail.com>
2021-07-22 12:29:16 +0900

    feat: add some numbers.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 console.log('Hello, Typescript!')
 
+let a: number = 3
+let b = a+3
+let c = {
+  apple: a,
+  banana: b
+}
+
+let d = c.apple * 4
+console.log('c.apple * 4 is ' + d)
+
+function squareOf(n:number) {
+  return n*n
+}
+console.log('squareOf(2) is ' + squareOf(2))
