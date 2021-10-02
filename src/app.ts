0c9063b2271415b421d7f01bf94654986eb62e43
kemokemo <t2wonderland@gmail.com>
2021-10-03 01:19:04 +0900

    feat: read, set IDs, write html files.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,52 @@
 import * as fs from 'fs'
-import * as cheerio from 'cheerio';
+import parse, { HTMLElement, Node } from 'node-html-parser';
 
+//
+// Functions
+// 
+function walkNode(nodes: Node[], boxID: String, counter: number) : number {
+  nodes.forEach((n)=>{
+    if(n.childNodes.length != 0){
+      counter = walkNode(n.childNodes, boxID, counter);
+    }
+
+    let elem = n as HTMLElement;
+    if(elem == undefined || elem == null){
+      return;
+    }
+    const cName = elem.classNames;
+    if(cName == undefined || !IsTargetClass(cName)){
+      return;
+    }
+
+    elem.setAttribute('id', `${boxID}-${cName}-${counter}`);
+    console.log(`class: ${cName}, id: ${boxID}-${cName}-${counter}`);
+    counter++;
+  })
+  return counter;
+}
+
+const myClasses = ['myBox', 'myCheck', 'myNote'];
+
+function IsTargetClass(cName: string): boolean {
+  return myClasses.includes(cName)
+}
+
+//
+// Main
+//
 let htmlData = fs.readFileSync("dist/source.html", "utf-8");
-const $ = cheerio.load(htmlData);
+const root = parse(htmlData);
 
-$('.myCheck').each((i,elem)=>{
-  $(elem).attr('id', 'myCheck'+(i+1));
-});
-console.log($.html());
+root.querySelectorAll('.myBox').forEach((elem, i)=>{
+  const boxID = `box${i+1}`;
+  elem.setAttribute('id', boxID)
 
-$('#myCheck2').attr('checked', 'true');
-console.log($.html());
+  if(elem.childNodes.length != 0){
+    walkNode(elem.childNodes, boxID, 1);
+  }
+})
+
+console.log(root.toString())
+
+fs.writeFileSync("dist/target.html", root.toString());
